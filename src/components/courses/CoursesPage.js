6e1945f76781f790811e944beca6383cfda6dc34
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-18 16:14:50 +0300

    Add add course button

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import * as courseActions from "../../redux/actions/courseActions";
@@ -7,6 +8,10 @@ import * as authorActions from "../../redux/actions/authorActions";
 import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false,
+  };
+
   componentDidMount() {
     const { courses, authors, actions } = this.props;
 
@@ -25,7 +30,15 @@ class CoursesPage extends React.Component {
   render() {
     return (
       <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
         <CourseList courses={this.props.courses} />
       </>
     );
